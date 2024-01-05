3a72ec9677d715e1a777041acd16c1df7a075cc3
michelevantaggi02 <michelevantaggi02@users.noreply.github.com>
2024-01-05 18:00:01 +0000

    On-push compilation

--- obj/Debug/net8.0/bot.AssemblyInfo.cs
+++ obj/Debug/net8.0/bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48793c1021afe06ab913f17569281a9791ee587e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca4d19c7c75091301742ae807af660e0f22a948")]
 [assembly: System.Reflection.AssemblyProductAttribute("bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
